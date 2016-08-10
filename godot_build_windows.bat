723eab4aa3d17910f4f40e9150b6fd2eea95ccb8
vipsbpig <vipsbpig@gmail.com>
2016-08-10 20:19:47 +0800

    fix compile script

--- godot_build_windows.bat
+++ godot_build_windows.bat
@@ -1,7 +1,6 @@
 @echo off
 SETLOCAL
  
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
  
 IF "%1" == "debug" goto build_DEBUG
 IF "%1" == "release" goto build_RELEASE
@@ -15,23 +14,30 @@ echo No Need To Build Debug !
 goto end
  
 :build_RELEASE_DEBUG
+
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
 echo Building Release_Debug 32bits!
-call scons platform=windows target=release_debug bits=32 tools=no -j 7
+call scons platform=windows target=release_debug tools=no -j 7
 copy ".\bin\godot.windows.opt.debug.32.exe" ".\templates\windows_32_debug.exe"
 call upx -9 ".\templates\windows_32_debug.exe"
+
 echo Building Release_Debug 64bits!
-call scons platform=windows target=release_debug bits=64 tools=no -j 7
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
+call scons platform=windows target=release_debug tools=no -j 7
 copy ".\bin\godot.windows.opt.debug.64.exe" ".\templates\windows_64_debug.exe"
 call upx -9 ".\templates\windows_64_debug.exe"
 goto end
  
 :build_RELEASE
 echo Building Release 32bits!
-call scons platform=windows target=release bits=32 tools=no -j 7
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
+call scons platform=windows target=release tools=no -j 7
 copy ".\bin\godot.windows.opt.32.exe" ".\templates\windows_32_release.exe"
 call upx -9 ".\templates\windows_32_release.exe"
 echo Building Release 64bits!
-call scons platform=windows target=release bits=64 tools=no -j 7
+
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
+call scons platform=windows target=release tools=no -j 7
 copy ".\bin\godot.windows.opt.64.exe" ".\templates\windows_64_release.exe"
 call upx -9 ".\templates\windows_64_release.exe"
 goto end
--- godot_build_windows_tools.bat
+++ godot_build_windows_tools.bat
@@ -1,7 +1,7 @@
 @echo off
 SETLOCAL
  
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
  
 IF "%1" == "debug" goto build_DEBUG
 IF "%1" == "release" goto build_RELEASE
@@ -11,14 +11,14 @@ goto end
  
 :build_DEBUG
 echo Building Debug 64bits!
-call scons platform=windows target=debug bits=64 -j 7
+call scons platform=windows target=debug -j 7
 copy ".\bin\godot.windows.tools.64.exe" ".\..\..\bin\godot.windows.tools.64.exe"
 call upx -9 ".\..\..\bin\godot.windows.tools.64.exe"
 goto end
  
 :build_RELEASE_DEBUG
 echo Building Release_Debug 64bits!
-call scons platform=windows target=release_debug bits=64 -j 7
+call scons platform=windows target=release_debug -j 7
 copy ".\bin\godot.windows.opt.tools.64.exe" ".\..\..\bin\godot.windows.opt.tools.64.exe"
 call upx -9 ".\..\..\bin\godot.windows.opt.tools.64.exe"
 goto end
