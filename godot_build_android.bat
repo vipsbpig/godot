04605854f9b46e3d8900c80e365902f27a222d8b
vipsbpig <vipsbpig@gmail.com>
2016-11-19 10:11:58 +0800

    godot build script update

--- godot_build_android.bat
+++ godot_build_android.bat
@@ -1,38 +1,35 @@
 @echo off
 SETLOCAL
- 
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
- 
+
 IF "%1" == "debug" goto build_DEBUG
 IF "%1" == "release" goto build_RELEASE
 IF "%1" == "release_debug" goto build_RELEASE_DEBUG
 IF "%1" == "all" goto build_ALL
- 
+
 goto end
- 
+
 :build_DEBUG
 echo No Need To Build Debug!
 
 goto end
- 
+
+
 :build_RELEASE_DEBUG
 echo Building Release_Debug!
 call scons platform=android target=release_debug tools=no -j 7
-::copy ".\bin\libgodot.android.opt.debug.armv7.neon.so" ".\platform\android\java\libs\armeabi\libgodot_android.so"
 cd platform/android/java/
-call gradlew.bat build
-::copy ".\build\outputs\apk\java-release-unsigned.apk" "..\..\..\templates\android_debug.apk"
+call gradlew.bat build --info > bugtags.log
 cd ..\..\..
+copy "bin\android_debug.apk"  "%USERPROFILE%\AppData\Roaming\Godot\templates\android_debug.apk"
 goto end
- 
+
 :build_RELEASE
 echo Building Release!
 call scons platform=android target=release tools=no -j 7
-::copy ".\bin\libgodot.android.opt.armv7.neon.so" ".\platform\android\java\libs\armeabi\libgodot_android.so"
 cd platform/android/java/
-call gradlew.bat build
-::copy ".\build\outputs\apk\java-release-unsigned.apk" "..\..\..\templates\android_release.apk"
+call gradlew.bat build --info > bugtags.log
 cd ..\..\..
+copy "bin\android_release.apk"  "%USERPROFILE%\AppData\Roaming\Godot\templates\android_release.apk"
 goto end
 
 :build_ALL
@@ -41,10 +38,10 @@ call scons platform=android target=release_debug tools=no -j 7
 echo Building Release!
 call scons platform=android target=release tools=no -j 7
 cd platform/android/java/
-call gradlew.bat build
+call gradlew.bat build 
 cd ..\..\..
-copy "bin\android_debug.apk"  "templates\android_debug.apk"
-copy "bin\android_release.apk" "templates\android_release.apk"
+copy "bin\android_debug.apk"  "%USERPROFILE%\AppData\Roaming\Godot\templates\android_debug.apk"
+copy "bin\android_release.apk"  "%USERPROFILE%\AppData\Roaming\Godot\templates\android_release.apk"
 
 :end
 ENDLOCAL
