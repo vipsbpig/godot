925f54e2baf04470dd26043ddd24681a1208d79b
vipsbpig <vipsbpig@gmail.com>
2016-11-21 19:01:23 +0800

    javascript complete

--- godot_build_javascript.bat
+++ godot_build_javascript.bat
@@ -16,23 +16,73 @@ goto end
 
 :build_RELEASE_DEBUG
 echo Building Release_Debug !
-call scons platform=javascript target=release_debug tools=no -j 7
+call scons platform=javascript target=release_debug tools=no module_openssl_enabled=no -j 7
 
+:zip_RELEASE_DEBUG
+mkdir templates\html_fs
+copy tools\dist\html_fs templates\html_fs
+copy bin\godot.javascript.opt.debug.exe.html.mem templates\html_fs
+copy bin\godot.javascript.opt.debug.exe.js templates\html_fs
+cd templates\html_fs
+rename godot.javascript.opt.debug.exe.html.mem godot.mem
+rename godot.javascript.opt.debug.exe.js godot.js
+7z a ..\javascript_debug.zip godot.html godot.js godot.mem godotfs.js
+cd ..\..
+rmdir /s/q templates\html_fs
 goto end
 
 :build_RELEASE
 echo Building Release !
-call scons platform=javascript target=release tools=no -j 7
+call scons platform=javascript target=release tools=no module_openssl_enabled=no -j 7
 echo Building Release 64bits!
+
+:zip_RELEASE
+mkdir templates\html_fs
+copy tools\dist\html_fs\godotfs.js templates\html_fs
+copy bin\godot.javascript.opt.exe.html templates\html_fs
+copy bin\godot.javascript.opt.exe.html.mem templates\html_fs
+copy bin\godot.javascript.opt.exe.js templates\html_fs
+cd templates\html_fs
+rename godot.javascript.opt.exe.html godot.html
+rename godot.javascript.opt.exe.html.mem godot.mem
+rename godot.javascript.opt.exe.js godot.js
+7z a ..\javascript_release.zip godot.html godot.js godot.mem godotfs.js
+cd ..\..
+rmdir /s/q templates\html_fs
 
 goto end
 
 :build_ALL
 echo Building Release_Debug !
-call scons platform=javascript target=release_debug tools=no -j 7
+call scons platform=javascript target=release_debug tools=no module_openssl_enabled=no -j 7
 
 echo Building Release !
-call scons platform=javascript target=release tools=no -j 7
+call scons platform=javascript target=release tools=no module_openssl_enabled=no -j 7
+
+
+mkdir templates\html_fs
+copy tools\dist\html_fs templates\html_fs
+copy bin\godot.javascript.opt.debug.exe.html.mem templates\html_fs
+copy bin\godot.javascript.opt.debug.exe.js templates\html_fs
+cd templates\html_fs
+rename godot.javascript.opt.debug.exe.html.mem godot.mem
+rename godot.javascript.opt.debug.exe.js godot.js
+7z a ..\javascript_debug.zip godot.html godot.js godot.mem godotfs.js
+cd ..\..
+rmdir /s/q templates\html_fs
+
+mkdir templates\html_fs
+copy tools\dist\html_fs\godotfs.js templates\html_fs
+copy bin\godot.javascript.opt.exe.html templates\html_fs
+copy bin\godot.javascript.opt.exe.html.mem templates\html_fs
+copy bin\godot.javascript.opt.exe.js templates\html_fs
+cd templates\html_fs
+rename godot.javascript.opt.exe.html godot.html
+rename godot.javascript.opt.exe.html.mem godot.mem
+rename godot.javascript.opt.exe.js godot.js
+7z a ..\javascript_release.zip godot.html godot.js godot.mem godotfs.js
+cd ..\..
+rmdir /s/q templates\html_fs
 
 :end
 ENDLOCAL
