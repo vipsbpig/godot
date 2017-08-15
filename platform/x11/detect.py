V-Sekai-fire/multiplayer-fabric-godot

enet: Split enet thirdparty files and allow unbundling
Building against shared libraries only implemented for Linux X11 so far.
TODO: Document Godot's modifications of upstream enet.

platform/x11/detect.py
@@ -149,7 +149,8 @@ def configure(env):
 	if (env["freetype"]=="yes"):
 		env.ParseConfig('pkg-config freetype2 --cflags --libs')
 
-
+	if (env["enet"] == "system"):
+		env.ParseConfig('pkg-config libenet --cflags --libs')
 
 
 	env.Append(CPPFLAGS=['-DOPENGL_ENABLED'])

vipsbpig/godot

platform/x11/detect.py
@@ -162,6 +162,10 @@ def configure(env):
     if (env['builtin_squish'] == 'no' and env["tools"] == "yes"):
         env.ParseConfig('pkg-config libsquish --cflags --libs')
 
+    if (env["builtin_enet"] == "no"):
+		env.ParseConfig('pkg-config libenet --cflags --libs')
+
+
     # Sound and video libraries
     # Keep the order as it triggers chained dependencies (ogg needed by others, etc.)
 