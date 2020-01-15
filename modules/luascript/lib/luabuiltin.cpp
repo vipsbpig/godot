07cd15aeca8583cb629240c680f1eecf3374fda7
vipsbpig <vipsbpig@gmail.com>
2020-01-15 15:54:12 +0800

    update call function

--- modules/luascript/lib/luabuiltin.cpp
+++ modules/luascript/lib/luabuiltin.cpp
@@ -4,205 +4,92 @@
 
 void LuaBuiltin::regitser_builtins(lua_State *L) {
 	LuaBindingHelper::stackDump(L);
-	const char *buildIns[] = {
-		"Vector2", //0
-		"Rect2", //1
-		"Color", //2
-		"Vector3", //3
-		"Basis", //4
-		"Quat", //5
-		"RID", //6
-		"Transform2D", //7
-		"Plane", //8
-		"AABB", //9
-		"Transform", //10
-		"PoolByteArray", //11
-		"PoolIntArray", //12
-		"PoolRealArray", //13
-		"PoolStringArray", //14
-		"PoolVector2Array", //15
-		"PoolVector3Array", //16
-		"PoolColorArray", //17
+	typedef struct {
+		const char *type;
+		Variant::Type vt;
+	} BulitinTypes;
+	const BulitinTypes buildIns[] = {
+		{ "Vector2", Variant::VECTOR2 }, //0
+		{ "Rect2", Variant::RECT2 }, //1
+		{ "Color", Variant::COLOR }, //2
+		{ "Vector3", Variant::VECTOR3 }, //3
+		{ "Basis", Variant::BASIS }, //4
+		{ "Quat", Variant::QUAT }, //5
+		{ "RID", Variant::_RID }, //6
+		{ "Transform2D", Variant::TRANSFORM2D }, //7
+		{ "Plane", Variant::PLANE }, //8
+		{ "AABB", Variant::AABB }, //9
+		{ "Transform", Variant::TRANSFORM }, //10
+		{ "PoolByteArray", Variant::POOL_BYTE_ARRAY }, //11
+		{ "PoolIntArray", Variant::POOL_INT_ARRAY }, //12
+		{ "PoolRealArray", Variant::POOL_REAL_ARRAY }, //13
+		{ "PoolStringArray", Variant::POOL_STRING_ARRAY }, //14
+		{ "PoolVector2Array", Variant::POOL_VECTOR2_ARRAY }, //15
+		{ "PoolVector3Array", Variant::POOL_VECTOR3_ARRAY }, //16
+		{ "PoolColorArray", Variant::POOL_COLOR_ARRAY }, //17
 	};
 
 	lua_getfield(L, LUA_GLOBALSINDEX, "GD");
-
-	int len = sizeof(buildIns) / sizeof(char *);
+	int len = sizeof(buildIns) / sizeof(BulitinTypes);
 	for (int i = 0; i < len; i++) {
 		lua_newtable(L);
 		char dst[20];
-		sprintf(dst, ".%s", buildIns[i]);
+		snprintf(dst, 20, ".%s", buildIns[i].type);
 		luaL_newmetatable(L, dst);
 		{
 			lua_pushstring(L, "__call");
-			lua_pushstring(L, buildIns[i]);
-			lua_pushinteger(L, i);
+			lua_pushinteger(L, buildIns[i].vt);
+			lua_pushstring(L, buildIns[i].type);
 			lua_pushcclosure(L, LuaBuiltin::meta_bultins__call, 2);
 			lua_rawset(L, -3);
 		}
 		lua_setmetatable(L, -2);
-		lua_setfield(L, -2, buildIns[i]);
+		lua_setfield(L, -2, buildIns[i].type);
 	}
-
 	lua_pop(L, 1);
 }
 
 int LuaBuiltin::meta_bultins__call(lua_State *L) {
-	const char *builtInType = luaL_checkstring(L, lua_upvalueindex(1));
-	int builtIn = lua_tointeger(L, lua_upvalueindex(2));
-	int args_c = lua_gettop(L);
-	print_format("%d type:%s", builtIn, builtInType);
+	Variant::Type type = (Variant::Type)lua_tointeger(L, lua_upvalueindex(1));
+	const char *type_name = lua_tostring(L, lua_upvalueindex(2));
+	int top = lua_gettop(L);
+	Variant::CallError err;
+	Variant ret;
+	if (top <= 1) {
 
-	if (0 == builtIn) { //Vector2
-		Vector2 *ptr;
-		switch (args_c) {
-			case 0:
-				ptr = new Vector2();
-				break;
-			case 1: {
-				Variant arg1;
-				l_get_variant(L, 1, arg1);
-				ptr = new Vector2(arg1);
-				break;
-			}
-			case 2: {
-				Variant arg1;
-				Variant arg2;
-				l_get_variant(L, 1, arg1);
-				l_get_variant(L, 2, arg2);
-				ptr = new Vector2(arg1, arg2);
-				break;
-			}
-			default: return 0; break;
-		}
-		l_push_bulltins_type(L, *ptr);
-		delete ptr;
-		return 1;
-
-	} else if (3 == builtIn) { //Vector3
-		Vector3 *ptr;
-		switch (args_c) {
-			case 0:
-				ptr = new Vector3();
-				break;
-			case 1: {
-				Variant arg1;
-				l_get_variant(L, 1, arg1);
-				ptr = new Vector3(arg1);
-				break;
-			}
-			case 3: {
-				Variant arg1;
-				Variant arg2;
-				Variant arg3;
-				l_get_variant(L, 1, arg1);
-				l_get_variant(L, 2, arg2);
-				l_get_variant(L, 3, arg3);
-				ptr = new Vector3(arg1, arg2, arg3);
-				break;
-			}
-			default: return 0; break;
-		}
-		l_push_bulltins_type(L, *ptr);
-		delete ptr;
-		return 1;
-
-	} else if (4 == builtIn) { //Basis
-		Basis *ptr;
-		switch (args_c) {
-			case 0:
-				ptr = new Basis();
-				break;
-			case 1: {
-				Variant arg1;
-				l_get_variant(L, 1, arg1);
-				ptr = new Basis(arg1);
-				break;
-			}
-			case 2: {
-				Variant arg1;
-				Variant arg2;
-				l_get_variant(L, 1, arg1);
-				l_get_variant(L, 2, arg2);
-				if (arg2.get_type() == Variant::VECTOR3)
-					ptr = new Basis((Vector3)arg1, (Vector3)arg2);
-				else
-					ptr = new Basis(arg1, (real_t)arg2);
-				break;
-			}
-			case 3: {
-				Variant arg1;
-				Variant arg2;
-				Variant arg3;
-				l_get_variant(L, 1, arg1);
-				l_get_variant(L, 2, arg2);
-				l_get_variant(L, 3, arg3);
-				if (arg2.get_type() == Variant::VECTOR3)
-					ptr = new Basis(arg1, (Vector3)arg2, arg3);
-				else
-					ptr = new Basis(arg1, (real_t)arg2, arg3);
-				break;
-			}
-			default: return 0; break;
-		}
-		l_push_bulltins_type(L, *ptr);
-		delete ptr;
-		return 1;
-
-	} else if (7 == builtIn) { //Transform2D
-		Transform2D *ptr;
-		switch (args_c) {
-			case 0:
-				ptr = new Transform2D();
-				break;
-			case 1: {
-				Variant arg1;
-				l_get_variant(L, 1, arg1);
-				ptr = new Transform2D(arg1);
-				break;
-			}
-			case 2: {
-				Variant arg1;
-				Variant arg2;
-				l_get_variant(L, 1, arg1);
-				l_get_variant(L, 2, arg2);
-				ptr = new Transform2D(arg1, arg2);
-				break;
-			}
-			default: return 0; break;
-		}
-		l_push_bulltins_type(L, *ptr);
-		return 1;
-		delete ptr;
-
-	} else if (10 == builtIn) { //Transform
-		Transform *ptr;
-		switch (args_c) {
-			case 0:
-				ptr = new Transform();
-				break;
-			case 1: {
-				Variant arg1;
-				l_get_variant(L, 1, arg1);
-				ptr = new Transform(arg1);
-				break;
-			}
-			case 2: {
-				Variant arg1;
-				Variant arg2;
-				l_get_variant(L, 1, arg1);
-				l_get_variant(L, 2, arg2);
-				ptr = new Transform(arg1, arg2);
-				break;
-			}
-			default: return 0; break;
-		}
-		l_push_bulltins_type(L, *ptr);
-		return 1;
-		delete ptr;
-
+		ret = Variant::construct(type, NULL, 0, err);
 	} else {
-		luaL_error(L, "TODO builtIn type:%s", builtInType);
+
+		Variant *vars = memnew_arr(Variant, top - 1);
+		Variant *args[128];
+		for (int idx = 2; idx <= top; idx++) {
+
+			Variant &var = vars[idx - 2];
+			args[idx - 2] = &var;
+			l_get_variant(L, idx, var);
+		}
+		ret = Variant::construct(type, (const Variant **)(args), top - 1, err);
+		memdelete_arr(vars);
+	}
+	switch (err.error) {
+		case Variant::CallError::CALL_OK:
+			l_push_variant(L, ret);
+			return 1;
+		case Variant::CallError::CALL_ERROR_INVALID_METHOD:
+			luaL_error(L, "Invalid method");
+			break;
+		case Variant::CallError::CALL_ERROR_INVALID_ARGUMENT:
+			luaL_error(L, "Invalid argument to construct built-in type '%s", type_name);
+			break;
+		case Variant::CallError::CALL_ERROR_TOO_MANY_ARGUMENTS:
+			luaL_error(L, "Too many arguments to construct built-in type '%s", type_name);
+			break;
+		case Variant::CallError::CALL_ERROR_TOO_FEW_ARGUMENTS:
+			luaL_error(L, "Too few arguments to construct built-in type '%s", type_name);
+			break;
+		case Variant::CallError::CALL_ERROR_INSTANCE_IS_NULL:
+			luaL_error(L, "Instance is null");
+			break;
 	}
 	return 0;
 }
