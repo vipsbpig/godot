ed8bb745111395a1f0aa9542248521c9b3a25560
vipsbpig <vipsbpig@gmail.com>
2020-01-15 20:58:36 +0800

    add some call fixed

--- modules/luascript/luascript_instance.cpp
+++ modules/luascript/luascript_instance.cpp
@@ -5,6 +5,7 @@ LuaScriptInstance::LuaScriptInstance() {
 }
 
 LuaScriptInstance::~LuaScriptInstance() {
+	print_format("LuaScriptInstance::descontruct");
 }
 
 Error LuaScriptInstance::init(bool p_ref) {
@@ -13,11 +14,14 @@ Error LuaScriptInstance::init(bool p_ref) {
 	//然后方便后续的调用
 	//script里面获取方法，instance的对象来调用
 	//这里要父类调用设置叫base吧
-	//
+	//需要创建LuaInstance的metatable ，用table还是userdata呢？
+	//不能创建新的变量，但是可以赋值
+	//赋值可以传递到引擎里面
+	//如果复制到自己就引擎就不可以得知
 
 	Object *obj = owner;
 	print_format("LuaScriptInstance::init obj type:%s script:%d", String(Variant(obj)).utf8().get_data(), script.ptr());
-	// call("_init", NULL, 0, Variant::CallError());
+	call("_init", NULL, 0, Variant::CallError());
 	return OK;
 }
 bool LuaScriptInstance::has_method(const StringName &p_method) const {
@@ -32,10 +36,15 @@ bool LuaScriptInstance::has_method(const StringName &p_method) const {
 }
 
 Variant LuaScriptInstance::call(const StringName &p_method, const Variant **p_args, int p_argcount, Variant::CallError &r_error) {
-	Variant var;
+	//TODO:: need to complete
 	print_format("LuaScriptInstance::call %s %d", String(p_method).utf8().get_data(), p_argcount);
-	if (script->has_method(p_method))
+	if (script->has_method(p_method)) {
+		Variant var;
 		LuaScriptLanguage::get_singleton()->binding->instance_call(this, p_method, p_args, p_argcount, var, r_error);
+		return var;
+	}
+
+	//find object in cls
 	const ClassDB::ClassInfo *top = script->cls;
 	while (top->inherits_ptr) {
 		if (top->method_map.has(p_method)) {
@@ -45,7 +54,7 @@ Variant LuaScriptInstance::call(const StringName &p_method, const Variant **p_ar
 		top = top->inherits_ptr;
 	}
 
-	return var;
+	return Variant();
 }
 
 Ref<Script> LuaScriptInstance::get_script() const {
