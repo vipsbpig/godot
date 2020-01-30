89339c56ed20453ad14b0e16d715be5f720a019f
vipsbpig <vipsbpig@gmail.com>
2020-01-30 17:42:03 +0800

    luajit use lib

--- modules/luascript/config.py
+++ modules/luascript/config.py
@@ -6,7 +6,19 @@ def can_build(env, platform):
     return True
 
 def configure(env):
-    pass
+    #pass
+    bits = env['bits']
+    lua_include_path = "#modules/luascript/lib/luajit/src"
+
+    if env['platform'] == 'windows':
+        env.Append(LIBPATH=['#modules/luascript/lib/luajit'])
+        if bits == '32':
+            env.Append(LINKFLAGS=['lua51_x86.lib'])
+        else:
+            env.Append(LINKFLAGS=['lua51_x64.lib'])
+
+
+
 
 def get_doc_classes():
 	return ["LuaScript"]
